5b0edc7b89456a03ed4631880ccc77da6c4e4a58
Mustafiz Kaifee Mumtaz <mustafiz.mumtaz@medibuddy.in>
2024-02-29 01:51:18 +0530

    fix sit meta data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Mustafiz Kaifee',
   author: 'Mustafiz Kaifee',
   headerTitle: 'MustafizKaifee',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A software engineer who aim to evolve, innovates, travel and inspire through technogies',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://mustafizkaifee.in',
+  siteRepo: 'https://github.com/Mustafiz04/portfolio',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
