543853a688d1818553c01cb460aac081e21b716a
Mustafiz Kaifee Mumtaz <mustafiz.mumtaz@medibuddy.in>
2024-03-02 04:59:02 +0530

    fix comment issues

--- components/comments/index.tsx
+++ components/comments/index.tsx
@@ -1,42 +1,42 @@
-import siteMetadata from '@/data/siteMetadata';
-import dynamic from 'next/dynamic';
+import siteMetadata from '@/data/siteMetadata'
+import dynamic from 'next/dynamic'
 import type { Blog } from 'contentlayer/generated'
 import { CoreContent } from 'pliny/utils/contentlayer'
 
 interface Props {
-  frontMatter: CoreContent<Blog>;
+  frontMatter: CoreContent<Blog>
 }
 
 const GiscusComponent = dynamic(
   () => {
-    return import('@/components/comments/Giscus');
+    return import('@/components/comments/Giscus')
   },
-  { ssr: false },
-);
+  { ssr: false }
+)
 
 const Comments = ({ frontMatter }: Props) => {
   // console.log(frontMatter)
-  let term;
-  switch (
-    siteMetadata?.comments.giscusConfig.mapping
-  ) {
-    case 'pathname':
-      term = frontMatter.slug;
-      break;
-    case 'url':
-      term = window.location.href;
-      break;
-    case 'title':
-      term = frontMatter.title;
-      break;
-  }
+  let term
+  // switch (
+  //   siteMetadata.comments?.giscusConfig.mapping
+  // ) {
+  //   case 'pathname':
+  //     term = frontMatter.slug;
+  //     break;
+  //   case 'url':
+  //     term = window.location.href;
+  //     break;
+  //   case 'title':
+  //     term = frontMatter.title;
+  //     break;
+  // }
   return (
-    <div id='comment'>
+    <div id="comment">
       {siteMetadata.comments && siteMetadata.comments.provider === 'giscus' && (
-        <GiscusComponent mapping={term} />
+        <GiscusComponent mapping="pathname" />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Comments;
+export default Comments
---
+++ data/giscus.js
@@ -0,0 +1,25 @@
+const giscusConfig = {
+  // Visit the link below, and follow the steps in the 'configuration' section
+  // https://giscus.app/
+  repo: 'Mustafiz04/portfolio',
+  repositoryId: 'R_kgDOLZWcgg',
+  category: 'General',
+  categoryId: 'DIC_kwDOLZWcgs4Cdp1n',
+  mapping: 'pathname', // supported options: pathname, url, title
+  reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+  // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+  metadata: '0',
+  // theme example: light, dark, dark_dimmed, dark_high_contrast
+  // transparent_dark, preferred_color_scheme, custom
+  theme: 'light',
+  // theme when dark mode
+  darkTheme: 'transparent_dark',
+  // If the theme option above is set to 'custom`
+  // please provide a link below to your custom theme css file.
+  // example: https://giscus.app/themes/custom_example.css
+  themeURL: '',
+  // This corresponds to the `data-lang="en"` in giscus's configurations
+  lang: 'en',
+}
+
+module.exports = giscusConfig
