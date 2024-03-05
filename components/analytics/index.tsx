b9a64d5cc05af8cd753818545dd018064f495085
Mustafiz Kaifee Mumtaz <mustafiz.mumtaz@medibuddy.in>
2024-03-05 21:03:10 +0530

    Added Google Analytics

---
+++ components/analytics/index.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import GA from './GoogleAnalytics'
+
+import analytics from '@/data/analytics'
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void
+    plausible?: (...args: any[]) => void
+    sa_event?: (...args: any[]) => void
+  }
+}
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const Analytics = () => {
+  return <>{isProduction && analytics.googleAnalytics.googleAnalyticsId && <GA />}</>
+}
+
+export default Analytics
---
+++ data/analytics.js
@@ -0,0 +1,7 @@
+const analytics = {
+  googleAnalytics: {
+    googleAnalyticsId: 'G-ESNTZ92D3Q', // e.g. G-XXXXXXX
+  }
+}
+
+module.exports = analytics
