b23629e8bdd9b7ecafdd380e963ab6d0e82cbbf8
Mustafiz Kaifee Mumtaz <mustafiz.mumtaz@medibuddy.in>
2024-03-06 00:23:31 +0530

    fix lint issue.

--- components/analytics/index.tsx
+++ components/analytics/index.tsx
@@ -14,7 +14,7 @@ declare global {
 const isProduction = process.env.NODE_ENV === 'production'
 
 const Analytics = () => {
-  return <>{analytics.googleAnalytics.googleAnalyticsId && <GA />}</>
+  return <>{isProduction && analytics.googleAnalytics.googleAnalyticsId && <GA />}</>
 }
 
 export default Analytics
