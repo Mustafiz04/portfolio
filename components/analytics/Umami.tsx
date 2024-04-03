e56b58612e45f67db8da9ff50916a6b2ec38254a
Mustafiz04 <kaifee000@gmail.com>
2024-04-04 00:53:38 +0530

    fix umami website id

--- components/analytics/Umami.tsx
+++ components/analytics/Umami.tsx
@@ -1,11 +1,11 @@
 import Script from 'next/script'
-import siteMetadata from '@/data/siteMetadata'
+import { umami } from '@/data/analytics'
 
 export default function Umami() {
   return (
     <Script
       async
-      data-website-id={siteMetadata.umamiWebsiteId}
+      data-website-id={umami.umamiWebsiteId}
       src="https://analytics.leohuynh.dev/script.js"
     />
   )
